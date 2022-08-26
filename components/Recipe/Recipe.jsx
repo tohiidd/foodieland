af8bfd6b0aaa851d8586204a730cef1ba8df39e8
tohid <tohidebrahimi17@gmail.com>
2022-08-26 14:29:18 +0430

    feat:add recipe  to homepage

---
+++ components/Recipe/Recipe.jsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+function Recipe() {
+  return (
+    <div className="flex flex-col basis-[85%] md:basis-[40%] lg:basis-[30%] xl:basis-[23%] h-[320px] pb-8  rounded-3xl relative">
+      <Link href={`/recipe/${id}`}>
+        <div className="h-[230px] hover:scale-105 transition-all rounded-3xl w-full">
+          <img
+            src={img}
+            alt="product"
+            className=" block w-full h-full rounded-3xl object-cover"
+          />
+        </div>
+      </Link>
+      <div className="w-[48px] h-[48px] bg-white rounded-full flex justify-center items-center absolute top-[12px] right-[16px] ">
+        <span
+          onClick={() => setLike((prev) => !prev)}
+          className="w-[20.25px] h-[18px] inline-block cursor-pointer"
+        >
+          {like ? icons.whiteHeat() : icons.redHeat()}
+        </span>
+      </div>
+      <Link href={`/recipe/${id}`}>
+        <div>
+          <h2 className="text-lg font-semibold mt-1 tracking-[-0.04em] leading-[26px] text-black">
+            {title}
+          </h2>
+        </div>
+      </Link>
+      <div className="flex items-center absolute bottom-2 mt-3 font-medium text-sm leading-[17px] tracking-[-0.02em] text-[rgba(0,0,0,0.6)]">
+        <div className="flex items-center mr-3">
+          <span className="inline-block pr-2">{icons.timer()}</span>
+          {cookTime}
+        </div>
+        <div className="flex items-center">
+          <span className="inline-block ">{icons.fork()}</span>
+          <span className="inline-block pr-2">{icons.knife()}</span>
+          {category}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Recipe;
--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ function HomePage() {
       <Categories />
       <Container>
         <div className="flex flex-col justify-center items-center">
-          <Title className="mb-[24px]"> Simple and tasty recipes</Title>
-          <Subtitle className="mb-[96px] sm:mb-[80px]">
+          <Title className="mb-[24px] ">Simple and tasty recipes</Title>
+          <Subtitle className="mb-[96px] sm:mb-[80px] ">
             Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqut enim ad minim
           </Subtitle>
@@ -29,6 +29,17 @@ function HomePage() {
               category={category}
             />
           ))}
+        </div>
+      </Container>
+      <Container>
+        <div className="my-8 px-4 xl:px-0 flex flex-col items-center justify-between xl:flex-row xl:text-left lg:flex-col lg:text-center md:flex-col text-center sm:flex-col">
+          <Title className=" xl:text-left xl:w-[567px] md:w-11/12 w-full md:mb-2 xl:mb-0">
+            Try this delicious recipe to make your day
+          </Title>
+          <Subtitle className="xl:text-left xl:w-[620px] lg:w-full w-[90%]">
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim
+          </Subtitle>
         </div>
       </Container>
     </div>
